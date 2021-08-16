8d64170a37afb05a515457669474855a24eda648
scotty-james <sfrew26@gmail.com>
2021-08-16 13:13:55 +0000

    update admin fields for blog app

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
-from .models import Post 
+from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('user', )
+
+    list_display = (
+        'title',
+        'user',
+        'created_on',
+        'status',
+    )
+
+    list_filter = (
+        'status',
+    )
+
+admin.site.register(Post, PostAdmin)
