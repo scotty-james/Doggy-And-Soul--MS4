d398fd6be513edc1fdc49d511ec58daecd004728
scotty-james <sfrew26@gmail.com>
2021-08-16 12:50:55 +0000

    create blog app, add models, urls, views and template

---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import get_object_or_404, redirect, render, reverse
+from django.contrib import messages
+
+from .models import Post
+
+
+def blog_posts(request):
+    # View to return all blog posts
+    posts = Post.objects.filter(status=1).order_by('-created_on')
+    drafts = Post.objects.filter(status=0).order_by('-created_on')
+
+    context = {
+        'posts': posts,
+        'drafts': drafts,
+    }
+
+    return render(request, 'blog/blog.html', context)
