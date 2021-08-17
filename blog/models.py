9d69c5af604a49e174f6f557a4d04dba1b5b0007
scotty-james <sfrew26@gmail.com>
2021-08-17 10:12:53 +0000

    create comments model, migrate

--- blog/models.py
+++ blog/models.py
@@ -19,3 +19,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    comment_content = models.TextField(null=False, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return self.comment_content
