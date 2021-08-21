446663f6aa67faaca303005a896cb13f1d37eabc
scotty-james <sfrew26@gmail.com>
2021-08-21 17:49:55 +0000

    fix error in settings.py

--- doggy_and_soul/settings.py
+++ doggy_and_soul/settings.py
@@ -121,17 +121,12 @@ WSGI_APPLICATION = 'doggy_and_soul.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.parse('postgres://xxdusqplixlxff:93b63fe3caa7069b531c7bc3e5873bdca0a169bcbe46ba831d9e8fbd3d5ae74e@ec2-34-240-75-196.eu-west-1.compute.amazonaws.com:5432/d90t7cmrfgj395')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
