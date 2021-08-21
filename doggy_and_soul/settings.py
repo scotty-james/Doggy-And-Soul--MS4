d6cafd7aab1fb706fcaf4677c2d5bc4c1af0285e
scotty-james <sfrew26@gmail.com>
2021-08-21 18:25:47 +0000

    deploy to heroku

--- doggy_and_soul/settings.py
+++ doggy_and_soul/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9$6@j6j@6$p^)_tdnek1geoytjim9+()sd5%j(n61ntd+nla1p'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['the-dog-shop.herokuapp.com', 'localhost']
 
