d58174d9b0e3886087230c9c2e829cd5b9e56ad9
scotty-james <sfrew26@gmail.com>
2021-08-21 17:44:24 +0000

    begin heroku deployment

--- doggy_and_soul/settings.py
+++ doggy_and_soul/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,11 +121,15 @@ WSGI_APPLICATION = 'doggy_and_soul.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse('postgres://xxdusqplixlxff:93b63fe3caa7069b531c7bc3e5873bdca0a169bcbe46ba831d9e8fbd3d5ae74e@ec2-34-240-75-196.eu-west-1.compute.amazonaws.com:5432/d90t7cmrfgj395')
 }
 
 
